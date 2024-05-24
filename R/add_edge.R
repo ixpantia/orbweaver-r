3fcc2c88054c50305574e11a4c5c3ac4a707a94c
andres <afquinteromoreano@gmail.com>
2024-05-23 23:43:49 -0500

    feat: Adds `add_edge` function

---
+++ R/add_edge.R
@@ -0,0 +1,27 @@
+#' @title Add an edge to a graph
+#'
+#' @description
+#' Adds an edge from one node to another in a
+#' a directed graph.
+#'
+#' NOTE: Not all graphs are able to be modified.
+#'
+#' @param graph A graph object
+#' @param from The id of the `from` node.
+#' @param to The id of the `to` node.
+#' @return The updated graph object
+#' @export
+add_edge <- function(graph, from, to) {
+  UseMethod("add_edge")
+}
+
+#' @export
+add_edge.DirectedGraph <- function(graph, from, to) {
+  graph$add_edge(from, to)
+  return(graph)
+}
+
+#' @export
+add_edge.DirectedAcyclicGraph <- function(graph, node_id, data) {
+  rlang::abort(err_unable_to_modify_dag)
+}
